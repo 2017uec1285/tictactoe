72b9d74f0a498319474675f297cf51b062324a80
2017uec1285 <2017uec1285@mnit.ac.in>
2021-01-06 23:57:37 +0530

    Dynamic runtime env added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React,{useState} from "react";
 import Board from './components/Board';
 import { calculateWinner } from "./helpers";
+import History from './components/History';
 
 import "./styles/root.scss";
 
@@ -29,12 +30,16 @@ const App= () => {
       return prev.concat({board:newBoard,isXNext:!last.isXNext});
     });
     setCurrentMove(prev=>prev+1);
-    };
+  };
+  const moveTo=move=>{
+    setCurrentMove(move);
+  };
   return (
   <div className="app">
     <h1>TIC TAC TOE</h1>
     <h2>{message}</h2>
-    <Board board={current.board} handleSquareClick={handleSquareClick}/>
+    <Board board={ current.board } handleSquareClick={ handleSquareClick }/>
+    <History history={ history } moveTo={ moveTo } currentMove={ currentMove }/>
   </div>
 );
 }
